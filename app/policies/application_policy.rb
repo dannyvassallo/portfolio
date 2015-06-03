1c0fb68ee79dbdd2199b3a88c13213db5f95e82b
dannyvassallo <danielvassallo87@gmail.com>
2015-06-02 20:40:40 -0400

    setup pundit

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,54 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    scope.where(:id => record.id).exists?
+  end
+
+  def create?
+    new?
+  end
+
+  def new?
+    user.present? && user.admin?
+  end
+
+  def update?
+    new?
+  end
+
+  def edit?
+    new?
+  end
+
+  def destroy?
+    new?
+  end
+
+  def scope
+    record.class
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope
+    end
+  end
+end
+
