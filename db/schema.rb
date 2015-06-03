7e899582ea04e754ae676b2b6d206ba23c9ae72a
dannyvassallo <danielvassallo87@gmail.com>
2015-06-02 22:55:13 -0400

    associated user with posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603011132) do
+ActiveRecord::Schema.define(version: 20150603024939) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150603011132) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
