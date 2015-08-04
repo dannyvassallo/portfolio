6fa334b0ba356cb48a1e1ccc417fd71c3a1ed32f
dannyvassallo <danielvassallo87@gmail.com>
2015-08-03 21:15:59 -0400

    navbar scrolls

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -128,3 +128,21 @@ $(function(){
         limit: 20
       });
 });
+
+jQuery(document).ready(function($) {
+    $(".scrollto").click(function(event) {
+        event.preventDefault(); 
+
+        var defaultAnchorOffset = 0;
+
+        var anchor = $(this).attr('data-attr-scroll');
+
+        var anchorOffset = $('#'+anchor).attr('data-scroll-offset');
+        if (!anchorOffset)
+            anchorOffset = defaultAnchorOffset; 
+
+        $('html,body').animate({ 
+            scrollTop: $('#'+anchor).offset().top - anchorOffset
+        }, 500);        
+    });
+});
